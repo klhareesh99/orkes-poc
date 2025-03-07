0a790605c59536db468ab9c8cb5a464c3c36f8cd
klhareesh99 <86102826+klhareesh99@users.noreply.github.com>
2025-03-07 12:18:57 +0530

    Added Orkes API client

--- src/main/java/com/demo/api/ApiApplication.java
+++ src/main/java/com/demo/api/ApiApplication.java
@@ -1,13 +1,35 @@
 package com.demo.api;
 
+import static com.demo.api.utils.Constants.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.netflix.conductor.sdk.workflow.executor.WorkflowExecutor;
+import io.orkes.conductor.client.ApiClient;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(ApiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			ApiClient apiClient = ApiClient.builder()
+					.basePath(ORKES_URL)
+					.credentials(KEY, SECRET)
+					.build();
+			WorkflowExecutor exceutor = new WorkflowExecutor(apiClient, 3000);
+			executor.initWorkers("com.demo.api.workers");
+		} catch (Exception e) {
+			logger.error("Error: {}", e.getMessage());
+		}
 	}
 
 }
